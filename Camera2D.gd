8f74cfe2340a91cabc69da0e36b0a007e9709b74
llindemann <leonie.lindemann.ll@gmail.com>
2024-09-22 18:19:32 +0200

    fix: warning of unused parameters

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 const SPEED = 10.0
 	
-func _physics_process(delta):
+func _process(_delta):
 	# Get the input direction and handle the camera movement
 	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
 	position += direction * SPEED
