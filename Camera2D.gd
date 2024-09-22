ff9f158e3ec5b76ff11924bdb35eee9014a10dfa
llindemann <leonie.lindemann.ll@gmail.com>
2024-09-22 19:15:05 +0200

    feat: camera movement only on x axis

--- Camera2D.gd
+++ Camera2D.gd
@@ -4,5 +4,5 @@ const SPEED = 5.0
 func _process(_delta):
 	# Get the input direction and handle the camera movement
 	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
-	position += direction * SPEED
+	position.x += direction.x * SPEED
 
